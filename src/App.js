d3ae8dffa7578bc2db88e73e129eb0207a0b9264
Terletska Olena <terletskaia@ukr.net>
2022-12-02 13:03:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Open-source code
+            Open-sourced code
           </a>
           , by Olena Terletska
         </small>
